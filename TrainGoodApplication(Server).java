3ab47acae4f36295d9a13d09695984c1f8a14b00
whattheci <sehyeon1954@gmail.com>
2017-10-21 00:38:41 +0900

    Create TrainGoodApplication(Server).java

---
+++ TrainGoodApplication(Server).java
@@ -0,0 +1,12 @@
+package com.sehyeon.trainGood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainGoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainGoodApplication.class, args);
+	}
+}
